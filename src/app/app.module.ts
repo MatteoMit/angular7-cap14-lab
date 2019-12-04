61390c3f06395aa80a84a3f168e5b76a62e68b7d
MatteoMit <matteo.mitrano@3mlinformatica.it>
2019-12-04 16:25:44 +0100

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms'; // 1
 
 import { AppComponent } from './app.component';
-import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 import { ReactiveForm2Component } from './reactive-form2/reactive-form2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ReactiveFormComponent,
     ReactiveForm2Component
   ],
   imports: [
